822e4ad6a855e303ba57603cebff8bb6a67b8b10
GaranceBou <garanceboussuge@gmail.com>
2024-03-20 10:48:40 +0100

    new one

--- src/App.jsx
+++ src/App.jsx
@@ -2,13 +2,18 @@ import { useState } from "react";
 import "./App.css";
 import Section from "./components/Section";
 import Header from "./components/Header";
+import data from "./assets/data.json";
 
 function App() {
   return (
     <>
       <div className="page">
         <Header />
-        <Section />
+        <div className="section">
+          {data.map((elem) => {
+            return <Section elem={elem} key={elem.category} />;
+          })}
+        </div>
       </div>
     </>
   );
--- src/components/Section.jsx
+++ src/components/Section.jsx
@@ -1,24 +1,16 @@
-import data from "../assets/data.json";
-
-const Section = () => {
+const Section = (props) => {
   return (
     <div className="site">
-      {data.map((elem) => {
-        return (
-          <div className="section">
-            <h2>{elem.category}</h2>
-            <article className="carrousel">
-              {elem.images.map((poster) => {
-                return (
-                  <div className="poster">
-                    <img src={poster} alt="movieposter" />
-                  </div>
-                );
-              })}
-            </article>
-          </div>
-        );
-      })}
+      <h2>{props.elem.category}</h2>
+      <article className="carrousel">
+        {props.elem.images.map((poster) => {
+          return (
+            <div className="poster">
+              <img src={poster} alt="movieposter" />
+            </div>
+          );
+        })}
+      </article>
     </div>
   );
 };
